86c277534ba33a952f92db0df5c2c2c168724474
Sheldon <sheldonramlal99@gmail.com>
2026-01-29 03:37:58 -0400

    menu closing issue

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { useRef } from 'react'
-
-
+import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from 'react'
+
+
 
 
 const Navbar = () => {
@@ -14,6 +15,22 @@ const Navbar = () => {
     const toggleMenu =() => {
         setIsOpen(prev => !prev);
     }
+
+    const menuRef = useRef(null)
+
+
+
+    const navigate = useNavigate();
+
+    const handleNav = (id) => {
+        toggleMenu();
+        navigate("/");
+
+        setTimeout(() => {
+            document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
+        }, 0);
+    };
+
 
     
    const tl = useRef(null);
@@ -25,7 +42,6 @@ const Navbar = () => {
         opacity: 0,
     });
 
-    // build timeline ONCE
     tl.current = gsap.timeline({ paused: true });
 
     tl.current.to(".menu-item", {
@@ -36,12 +52,24 @@ const Navbar = () => {
         ease: "power3.out",
         delay: 0.2,
     });
-    }, []); // 👈 IMPORTANT: empty dependency array
+    }, []); 
 
     useEffect(() => {
         if (!tl.current) return;
 
         isOpen ? tl.current.play() : tl.current.reverse();
+        const handleClickOutside = (event) => {
+        // If menu is open and click is outside the menu
+         if (isOpen && menuRef.current && !menuRef.current.contains(event.target)) {
+            setIsOpen(false);
+         }
+  };
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
     }, [isOpen]);
 
 
@@ -50,19 +78,19 @@ const Navbar = () => {
 
   return (
     <>
-    <div className='absolute top-0  flex justify-end w-full text-white border-b border-[#2a2a2a] z-50 px-5 py-4 md:px-16'>
-       {/* menu icon */}
-        <div onClick={toggleMenu} className='cursor-pointer '>
-            <svg width="30" height="30" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7.94971 11.9497H39.9497" stroke="#fff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M7.94971 23.9497H39.9497" stroke="#fff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M7.94971 35.9497H39.9497" stroke="#fff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
+        <div  className='absolute top-0  flex justify-end w-full text-white border-b border-[#2a2a2a] z-50 px-5 py-4 md:px-16'>
+        {/* menu icon */}
+            <div onClick={toggleMenu} className='cursor-pointer '>
+                <svg width="30" height="30" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7.94971 11.9497H39.9497" stroke="#fff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+                    <path d="M7.94971 23.9497H39.9497" stroke="#fff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+                    <path d="M7.94971 35.9497H39.9497" stroke="#fff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
+            </div>
         </div>
-    </div>
         
 
-            <div className={`fixed top-0 right-0 z-50 w-full md:w-1/3 h-screen bg-[#0f0f0f]  transition transform duration-700 ease-[cubic-bezier(0.16,1,0.3,1)] ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+            <div ref={menuRef} className={`fixed top-0 right-0 z-50 w-full md:w-1/3 h-screen bg-[#0f0f0f]  transition transform duration-700 ease-[cubic-bezier(0.16,1,0.3,1)] ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
                 
                 <div className='flex flex-row px-5 h-16 items-center md:pr-16  justify-between border-b border-[#2a2a2a] ' >
 
@@ -83,21 +111,21 @@ const Navbar = () => {
 
                 </div>
                 <nav className="flex justify-center w-full h-full text-white ">
-                    <ul className='flex flex-col  p-10 w-full  h-3/4  justify-around   font-switzer *:text-4xl *:md:text-6xl *:py-10 *:border-b *:border-dashed *:border-[#2a2a2a] '>
+                    <ul className='flex flex-col  px-10 w-full  h-3/4     font-switzer *:text-4xl *:md:text-5xl *:py-8 *:border-b *:border-dashed *:border-[#2a2a2a] '>
                         <li className="menu-item">
-                            <a href="/#home" onClick={toggleMenu}>Home</a>
+                             <button onClick={() => handleNav("home")}>Home</button>
                         </li>
 
                         <li className="menu-item">
-                            <a href="/#about" onClick={toggleMenu}>About</a>
+                             <button onClick={() => handleNav("about")}>About</button>
                         </li>
 
                         <li className="menu-item">
-                            <a href="/#work" onClick={toggleMenu}>Work</a>
+                             <button onClick={() => handleNav("work")}>Work</button>
                         </li>
 
                         <li className="menu-item">
-                            <a href="/#contact" onClick={toggleMenu}>Contact</a>
+                             <button onClick={() => handleNav("contact")}>Contact</button>
                         </li>
 
                         <li className="menu-item">
